e5dfe773d9d657f3567a2dc6c932dfdb966b88f8
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-10 14:50:02 +0300

    fixed userModel

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-const appSchema = require('./app.model');
-
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+// import appSchema from "./app.model.js";
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, 
-  apps: [appSchema]  
+  password: { type: String, required: true },
+  // apps: [appSchema],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User;
