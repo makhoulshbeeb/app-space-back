d87f19aa03318984e473f9e7414713395457e201
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-10 12:41:25 +0300

    modified app model

--- models/app.model.js
+++ models/app.model.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const appSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  rating: { type: Number, required: true },
+  rating: { type: Number, min:1 ,max:5, required: true },
   reviews: { type: String, required: true },
 });
 
