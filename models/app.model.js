b10f7c548a765eb8a06eabad4296418cbab49c6c
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-10 14:50:16 +0300

    fixed appmodel

--- models/app.model.js
+++ models/app.model.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
+import mongoose from "mongoose";
+import { Schema } from "mongoose";  
 
 const appSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  rating: { type: Number, min:1 ,max:5, required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
   reviews: { type: String, required: true },
 });
-
-module.exports = appSchema;
+const App = mongoose.model("App", appSchema);
+export default App;
