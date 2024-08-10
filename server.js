efd1d5be8d7be0023cac4ec4261bc58bd632394b
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-10 14:43:02 +0300

    added user routes in the server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import databaseConnection from './config/db.js';
-
+import userRoutes from './routes/userRoutes.js';
 const server = express();
 
 dotenv.config();
 server.use(cors()); 
 server.use(express.json());
 
+server.use("/api",userRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, async () => {
