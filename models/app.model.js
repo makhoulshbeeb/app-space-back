dbfff9682a8b7c3ff167f91e1079c6df32619960
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-10 12:39:58 +0300

    fixeed model

--- models/app.model.js
+++ models/app.model.js
@@ -6,14 +6,7 @@ const appSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   rating: { type: Number, required: true },
-  reviews: [
-    {
-      user_id: { type: Schema.Types.ObjectId, ref: 'User' },
-      review: { type: String },
-      rating: { type: Number },
-      timestamp: { type: Date, default: Date.now }
-    }
-  ]
+  reviews: { type: String, required: true },
 });
 
 module.exports = appSchema;
