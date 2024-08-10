65bcc3413dc9c528e12cdc0ba34f758ac1d0864a
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-10 15:07:36 +0300

    added bycrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,42 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 // import appSchema from "./app.model.js";
-
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: "Please provide a valid email",
+    },
+  },
+
   password: { type: String, required: true },
   // apps: [appSchema],
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
