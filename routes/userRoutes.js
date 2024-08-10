2b52d07202484b6583c2c1e8210de0525ffdb4c7
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-10 15:08:16 +0300

    added bycrypt to the userModel

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,10 @@
 import express from "express";
-import {
-  registerUser,
-  // loginUser,
-} from "../controllers/userController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 // import { protect } from "../utils/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-// router.post("/login", loginUser);
-
+router.post("/login", loginUser);
 
 export default router;
