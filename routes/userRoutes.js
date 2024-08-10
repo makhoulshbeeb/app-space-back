1ae7f06f1897927f24c26794c5ca985553fe2fe9
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-10 16:58:27 +0300

    changed in the routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
-// import { protect } from "../utils/authMiddleware.js";
+import {
+  registerUser,
+  loginUser,
+
+
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
