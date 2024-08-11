3e9de5432a5ba84a2aff888967b1f6d056351c84
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-11 13:51:56 +0300

    added routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,13 @@ import express from "express";
 import {
   registerUser,
   loginUser,
-
-
+  getUserByName,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/user/:username", getUserByName);
 
 export default router;
