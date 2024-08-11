0d132712b6cc1fb433c97e472492be500fd109ec
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-11 13:23:46 +0300

    added category attribute

--- models/app.model.js
+++ models/app.model.js
@@ -3,6 +3,10 @@ import { Schema } from "mongoose";
 
 export const appSchema = new Schema({
   name: { type: String, required: true },
+  category: {
+    category_id: { type: String, required: true },
+    category_name: { type: String, required: true },
+  },
   price: { type: Number, required: true },
   rating: { type: Number, min: 1, max: 5, required: true },
   reviews: { type: String, required: true },
