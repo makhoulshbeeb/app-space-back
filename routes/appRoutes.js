182572fb24fece5fd48fdc77a8083958d6c929d8
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-11 13:04:30 +0300

    added routes to the app

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createApp, getApp, getAllApps } from "../controllers/appController.js";
+import { createApp, getApp, getAllApps, getAppBySearch } from "../controllers/appController.js";
 
 const router = express.Router();
 
 router.get("/app/:id", getApp);
 router.get("/app", getAllApps);
 router.post("/app", createApp);
+router.get("/app/search/:search", getAppBySearch);
+
 export default router;
